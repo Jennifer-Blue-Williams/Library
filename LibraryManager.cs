931616b08062fa00f9a5b0f77e2da2d1edfbf60b
Jennifer Williams <jennifer.blue.williams@gmail.com>
2022-03-04 15:58:45 -0600

    Completed Phase 9

---
+++ LibraryManager.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+// Create another file called LibraryManager.cs and create another class in it called LibraryManager. 
+namespace Library
+// Add a constructor method to your LibraryManager class that takes a string parameter that will represent the library's name.
+{
+    public class LibraryManager
+    {
+        public LibraryManager(string )
+        {
+            Name = name;
+        }
